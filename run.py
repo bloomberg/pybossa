65cf687f8f218009ddec7208b83627b462b68a62
ffiordalisi <ffiordalisi@bloomberg.net>
2018-09-06 09:48:54 -0400

    test

--- run.py
+++ run.py
@@ -17,6 +17,8 @@
 # along with PYBOSSA. If not, see <http://www.gnu.org/licenses/>.
 from pybossa.core import create_app
 
+print('oi')
+
 if __name__ == "__main__":  # pragma: no cover
     app = create_app()
     #logging.basicConfig(level=logging.NOTSET)
