c39c7d93c5c7232fc4d7da043ef5620b4f3154d5
Xi Chen <xchen375@bloomberg.net>
2020-10-28 14:37:32 -0400

    fix test case

--- test/test_api/test_project_api.py
+++ test/test_api/test_project_api.py
@@ -1310,6 +1310,25 @@ class TestProjectAPI(TestAPI):
         assert error['exception_msg'] == "Reserved keys in payload", error
 
     @with_context
+    def test_project_post_with_published_attribute_requires_password(self):
+        user = UserFactory.create()
+        data = dict(
+            name='name',
+            short_name='name',
+            description='description',
+            owner_id=user.id,
+            long_description=u'Long Description\n================',
+            info={'task_presenter': '<div>'},
+            published=True)
+        data = json.dumps(data)
+
+        res = self.app.post('/api/project?api_key=' + user.api_key, data=data)
+
+        error_msg = json.loads(res.data)['exception_msg']
+        assert res.status_code == 400, res.status_code
+        assert error_msg == 'password required', res.data
+
+    @with_context
     def test_project_update_with_published_attribute_is_not_forbidden(self):
         user = UserFactory.create()
         project = ProjectFactory.create(owner=user)
