7afb9f2f9b95a7bee6480a0b6436b697565389c5
pwills11 <pwills11@bloomberg.net>
2021-02-01 16:02:09 -0500

    updated error message

--- test/test_web.py
+++ test/test_web.py
@@ -8488,7 +8488,7 @@ class TestWeb(web.Helper):
         data = json.loads(res.data)
         next_url = '/project/{}/publish'.format(project.short_name)
 
-        assert data['flash'] == 'Error: Ensure PROD API key is used, your PROD account is Subadmin and enabled, and target project is enabled for project syncing', data
+        assert data['flash'] == 'Error: Ensure your production API key is used, your production account is sub-admin and enabled, and the target project is enabled for project syncing', data
         assert data['next'] == next_url, data
         assert data['status'] == 'error', data
 
