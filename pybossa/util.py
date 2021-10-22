57bc8c832f93985b819cbf39de07fdbe59de0341
Paxton Wills <pwills11@bloomberg.net>
2021-10-22 10:40:25 -0400

    email fix for util.py (#635)
    * edit metadata use type
    
    * add user.get('info', {}).get('metadata', {}).get('user_type')
    
    * remove info dict check
    
    * add metadata dict to test case

--- pybossa/util.py
+++ pybossa/util.py
@@ -1193,7 +1193,7 @@ def description_from_long_description(desc, long_desc):
 
 def generate_bsso_account_notification(user):
     from pybossa.core import user_repo
-    warning = False if user.get('user_type') else True
+    warning = False if user.get('metadata', {}).get('user_type') else True
     if warning:
         admins_email_list = [u.email_addr for u in user_repo.filter_by(admin=True)]
         template_type = 'adminbssowarning'
