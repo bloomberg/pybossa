647fef8054132d90ca376154e9754cd51c05bd3f
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2021-10-26 11:45:00 -0400

    RDISCROWD-4740 Use alert_list for bsso warnings and errors (#637)
    * Use ALERT_LIST for BSSO warnings and errors.
    
    * Comments in unit test.
    
    * Added missing user type to subject. Added unit test.
    
    * Quotes.

--- pybossa/util.py
+++ pybossa/util.py
@@ -1194,12 +1194,8 @@ def description_from_long_description(desc, long_desc):
 def generate_bsso_account_notification(user):
     from pybossa.core import user_repo
     warning = False if user.get('metadata', {}).get('user_type') else True
-    if warning:
-        admins_email_list = [u.email_addr for u in user_repo.filter_by(admin=True)]
-        template_type = 'adminbssowarning'
-    else:
-        admins_email_list = current_app.config.get('ALERT_LIST',[])
-        template_type = 'adminbssonotification'
+    template_type = 'adminbssowarning' if warning else 'adminbssonotification'
+    admins_email_list = current_app.config.get('ALERT_LIST',[])
 
     template = '/account/email/{}'.format(template_type)
 
@@ -1207,7 +1203,8 @@ def generate_bsso_account_notification(user):
     server_url = current_app.config.get('SERVER_URL')
     brand = current_app.config.get('BRAND')
 
-    subject = 'A new account has been created via BSSO for {}'.format(brand)
+    warning_msg = ' (missing user type)' if warning else ''
+    subject = 'A new account has been created via BSSO for {}{}'.format(brand, warning_msg)
     msg = dict(subject=subject,
                recipients=admins_email_list)
 
