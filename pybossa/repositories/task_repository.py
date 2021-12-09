eba3b8c22ea6ce339ac3d5a1cd084cdf4447cedd
pwills11 <pwills11@bloomberg.net>
2021-12-09 10:41:20 -0500

    clean up task_repository.py

--- pybossa/repositories/task_repository.py
+++ pybossa/repositories/task_repository.py
@@ -187,10 +187,10 @@ class TaskRepository(Repository):
     def update(self, element):
         self._validate_can_be(self.UPDATE_ACTION, element)
         try:
+            self.db.session.merge(element)
             self.db.session.commit()
             cached_projects.clean_project(element.project_id)
         except IntegrityError as e:
-            self.db.session.rollback()
             raise DBIntegrityError(e)
 
     def delete(self, element):
@@ -279,7 +279,7 @@ class TaskRepository(Repository):
         self._delete_zip_files_from_store(project)
 
     def get_tasks_by_filters(self, project, filters=None):
-         filters = filters or {}
+        filters = filters or {}
         conditions, params = get_task_filters(filters)
 
         sql = ''' SELECT task.id
