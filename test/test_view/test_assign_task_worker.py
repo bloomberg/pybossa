4abbeb52867b36fb2403645972b04587a5a538dc
pwills11 <pwills11@bloomberg.net>
2021-12-16 10:59:19 -0500

    fix plural to singular users typ0

--- test/test_view/test_assign_task_worker.py
+++ test/test_view/test_assign_task_worker.py
@@ -46,7 +46,7 @@ class TestAssignTaskWorker(web.Helper):
         assert res_data['assign_users'][0]['fullname'] == user.fullname
 
         all_user_emails = [u['email'] for u in res_data['all_users']]
-        assert user.email_addr not in all_users_emails, "existing users should be excluded from user list"
+        assert user.email_addr not in all_user_emails, "existing users should be excluded from user list"
 
     @with_context
     def test_get_users_single_task_2(self):
@@ -67,4 +67,64 @@ class TestAssignTaskWorker(web.Helper):
         assert res_data['assign_users'][0]['fullname'] == user.fullname
 
         all_user_emails = [u['email'] for u in res_data['all_users']]
-        assert invalid_email_addr not in all_users_emails, "existing users should be excluded from user list"
+        assert invalid_email_addr not in all_user_emails, "existing users should be excluded from user list"
+
+
+
+
+
+    # @with_context
+    # def test_get_users_multi_task_0(self):
+    #     """Test a single task without assign_user."""
+    #     project = ProjectFactory.create(published=True)
+    #     task = TaskFactory.create(project=project)
+    #     task = TaskFactory.create(project=project)
+
+    #     url = '/project/%s/tasks/assign-workersupdate?api_key=%s' % (project.short_name, project.owner.api_key)
+    #     req_data = dict(taskId=str(task.id))
+    #     res = self.app.post(url, content_type='application/json',
+    #       assign-workersupdate                  data=json.dumps(req_data))
+    #     res_data = json.loads(res.data)
+    #     assert not res_data['assign_users']
+    #     assert len(res_data['all_users']) == len(user_repo.get_all())
+
+    # @with_context
+    # def test_get_users_multi_task_1(self):
+    #     """Test a single task with assign_user."""
+    #     project = ProjectFactory.create(published=True)
+    #     user = UserFactory.create(email_addr='a@a.com', fullname="test_user")
+    #     task_user_pref = dict(assign_user=[user.email_addr])
+    #     task = TaskFactory.create(project=project, user_pref=json.dumps(task_user_pref))
+
+    #     url = '/project/%s/tasks/assign-workersupdate?api_key=%s' % (project.short_name, project.owner.api_key)
+    #     req_data = dict(taskId=str(task.id))
+    #     res = self.app.post(url, content_type='application/json',
+    #                         data=json.dumps(req_data))
+    #     res_data = json.loads(res.data)
+    #     assert len(res_data['assign_users']) == 1, res_data['assign_users']
+    #     assert res_data['assign_users'][0]['email'] == user.email_addr
+    #     assert res_data['assign_users'][0]['fullname'] == user.fullname
+
+    #     all_user_emails = [u['email'] for u in res_data['all_users']]
+    #     assert user.email_addr not in all_users_emails, "existing users should be excluded from user list"
+
+    # @with_context
+    # def test_get_users_single_task_2(self):
+    #     """Test a single task with assign_user invalid email addr."""
+    #     project = ProjectFactory.create(published=True)
+    #     user = UserFactory.create(email_addr='a@a.com', fullname="test_user")
+    #     invalid_email_addr = "invalid@email"
+    #     task_user_pref = dict(assign_user=[user.email_addr, invalid_email_addr])
+    #     task = TaskFactory.create(project=project, user_pref=json.dumps(task_user_pref))
+
+    #     url = '/project/%s/tasks/assign-workersupdate?api_key=%s' % (project.short_name, project.owner.api_key)
+    #     req_data = dict(taskId=str(task.id))
+    #     res = self.app.post(url, content_type='application/json',
+    #                         data=json.dumps(req_data))
+    #     res_data = json.loads(res.data)
+    #     assert len(res_data['assign_users']) == 2, res_data['assign_users']
+    #     assert res_data['assign_users'][0]['email'] == user.email_addr
+    #     assert res_data['assign_users'][0]['fullname'] == user.fullname
+
+    #     all_user_emails = [u['email'] for u in res_data['all_users']]
+    #     assert invalid_email_addr not in all_users_emails, "existing users should be excluded from user list"
