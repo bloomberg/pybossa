eb2e629f1e42c1d7c61dd598fb2800c6579f93f5
Xi Chen <xichen.u@gmail.com>
2022-03-01 12:25:05 -0500

    RDISCROWD-4967 Optimize slow queries used by Gigwork poller (#698)
    * RDISCROWD-4967 Optimize slow queries used by Gigwork poller
    
    * address code review comment
    
    * add a comment
    
    * add more comments

---
+++ alembic/versions/ed775af5e086_rdiscrowd_4967_add_partial_index_for_.py
@@ -0,0 +1,33 @@
+"""RDISCROWD-4967 add partial index for task table
+
+Revision ID: ed775af5e086
+Revises: 29c0cc545d6d
+Create Date: 2022-02-28 15:01:53.582867
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ed775af5e086'
+down_revision = '29c0cc545d6d'
+
+from alembic import op
+
+
+def upgrade():
+    # Workaround of "CREATE INDEX CONCURRENTLY cannot run inside a transaction block" exception
+    op.execute('COMMIT')
+
+    # passing all columns to avoid DB trip to query original table making indexing efficient.
+    # when new columns are added and used in the query, the index might need to be rebuilt.
+    op.create_index('task_state_calibration_exported_idx', 'task',
+                    ['id', 'created', 'project_id', 'state', 'quorum',
+                     'calibration', 'priority_0', 'info', 'n_answers',
+                     'fav_user_ids', 'exported', 'user_pref', 'worker_pref',
+                     'worker_filter', 'gold_answers', 'expiration'],
+                    postgresql_where="(state = 'completed'::text OR calibration = 1) AND exported = false",
+                    postgresql_concurrently=True
+                    )
+
+
+def downgrade():
+    op.drop_index('task_state_calibration_exported_idx')
