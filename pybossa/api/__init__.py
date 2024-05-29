2dd041b4697e380c007fed0dffa49b84abcc4028
Peter <peter@peterkle.com>
2024-05-29 09:40:14 -0400

    Update __init__.py

--- pybossa/api/__init__.py
+++ pybossa/api/__init__.py
@@ -707,8 +707,8 @@ def get_service_request(task_id, service_name, major_version, minor_version):
         if isinstance(service, dict):
             url = '{}/{}/{}/{}'.format(proxy_service_config['uri'], service_name, major_version, minor_version)
             headers = service.get('headers')
-            ssl_cert = current_app.config.get('SSL_CERT_PATH')
-            ret = requests.post(url, headers=headers, json=payload['data'], verify=ssl_cert if ssl_cert else True)
+            ssl_cert = current_app.config.get('SSL_CERT_PATH', True)
+            ret = requests.post(url, headers=headers, json=payload['data'], verify=ssl_cert)
             return Response(ret.content, 200, mimetype="application/json")
 
     current_app.logger.info(
