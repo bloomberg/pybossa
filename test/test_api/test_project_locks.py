6cc7a7145ba39a541f408f34195d6451feaba3ee
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2024-06-20 10:38:14 -0400

    RDISCROWD-7409 Locks API (#933)
    * Added /api/locks/{project_id}
    
    * Added project_locks.py
    
    * fix
    
    * unit test
    
    * unit tests
    
    * unit tests
    
    * unit test
    
    * unit test
    
    * Unit test for subadmin not in owners.
    
    * fix
    
    * save test project
    
    * fix
    
    * fix worker

--- test/test_api/test_project_locks.py
+++ test/test_api/test_project_locks.py
@@ -20,18 +20,23 @@ from unittest.mock import patch, call
 
 from nose.tools import assert_equal
 
+from unittest.mock import patch
+
 from pybossa.model.project import Project
+from pybossa.repositories import ProjectRepository, UserRepository
+from pybossa.api.project_locks import ProjectLocksAPI
 from test import db, with_context
 from test.factories import (ProjectFactory, UserFactory)
 from test.test_api import TestAPI
 
 
-
-class TestProjectAPI(TestAPI):
+class TestProjectLocksAPI(TestAPI):
 
     def setUp(self):
-        super(TestProjectAPI, self).setUp()
+        super(TestProjectLocksAPI, self).setUp()
         db.session.query(Project).delete()
+        self.project_repo = ProjectRepository(db)
+        self.user_repo = UserRepository(db)
 
     def setupProjects(self):
         project = ProjectFactory.create(
@@ -55,13 +60,21 @@ class TestProjectAPI(TestAPI):
         })
         return project
 
+    def _create_dict_from_model(self, item):
+        # Dummy method for testing
+        return {}
+
+    def _sign_item(self, item):
+        # Dummy method for testing
+        pass
+
     @with_context
-    def test_project_details_user_not_logged_in(self):
+    def test_project_locks_user_not_logged_in(self):
         """ Test should return 401 if the user is not logged in"""
         project = self.setupProjects()
         project_id = str(project.id)
 
-        res = self.app.get('/api/projectdetails/' + project_id)
+        res = self.app.get('/api/locks/' + project_id)
         err = json.loads(res.data)
         assert res.status_code == 401, err
         assert err['status'] == 'failed', err
@@ -70,15 +83,18 @@ class TestProjectAPI(TestAPI):
         assert err['action'] == 'GET', err
 
     @with_context
-    def test_project_details_user_worker(self):
+    def test_project_locks_user_worker(self):
         """ Test API should return 401 if user is worker"""
-        admin = UserFactory.create(admin=True)
         worker = UserFactory.create(admin=False, subadmin=False)
 
+        # Remove admin from worker.
+        worker.admin = False
+        self.user_repo.save(worker)
+
         project = self.setupProjects()
         project_id = str(project.id)
 
-        res = self.app.get('/api/projectdetails?id=' + project_id + '&api_key=' + worker.api_key + '&all=1')
+        res = self.app.get('/api/locks?id=' + project_id + '&api_key=' + worker.api_key + '&all=1')
         err = json.loads(res.data)
         assert res.status_code == 401, err
         assert err['status'] == 'failed', err
@@ -87,123 +103,104 @@ class TestProjectAPI(TestAPI):
         assert err['action'] == 'GET', err
 
     @with_context
-    def test_project_details_user_subadmin(self):
+    def test_project_locks_user_subadmin(self):
         """ Test API should work if user is subadmin"""
-        admin = UserFactory.create(admin=True)
         subadmin = UserFactory.create(admin=False, subadmin=True)
-
         project = self.setupProjects()
-        project_id = str(project.id)
 
-        res = self.app.get('/api/projectdetails?id=' + project_id + '&api_key=' + subadmin.api_key + '&all=1')
+        # Remove admin from subadmin.
+        subadmin.admin = False
+        self.user_repo.save(subadmin)
+
+        # Assign subadmin as owner of this project.
+        project.owners_ids.append(subadmin.id)
+        self.project_repo.save(project)
+
+        project_id = str(project.id)
+        res = self.app.get('/api/locks?id=' + project_id + '&api_key=' + subadmin.api_key + '&all=1')
         data = json.loads(res.data)
         assert res.status_code == 200, data
-        assert data[0]['product'] == 'test_product', data
         assert data[0]['short_name'] == 'test-app1', data
 
     @with_context
-    def test_project_details_user_admin(self):
+    def test_project_locks_user_subadmin_not_owner(self):
+        """ Test API should not work if user is subadmin but no in project owners"""
+        subadmin = UserFactory.create(admin=False, subadmin=True)
+
+        # Remove admin from subadmin.
+        subadmin.admin = False
+        self.user_repo.save(subadmin)
+
+        project = self.setupProjects()
+        project_id = str(project.id)
+
+        res = self.app.get('/api/locks?id=' + project_id + '&api_key=' + subadmin.api_key + '&all=1')
+        err = json.loads(res.data)
+        assert res.status_code == 401, err
+        assert err['status'] == 'failed', err
+        assert err['target'] == 'project', err
+        assert err['exception_cls'] == 'Unauthorized', err
+        assert err['action'] == 'GET', err
+
+    @with_context
+    def test_project_locks_user_admin(self):
         """ Test API should work if user is admin"""
         admin = UserFactory.create(admin=True)
 
         project = self.setupProjects()
         project_id = str(project.id)
 
-        res = self.app.get('/api/projectdetails?id=' + project_id + '&api_key=' + admin.api_key + '&all=1')
+        res = self.app.get('/api/locks?id=' + project_id + '&api_key=' + admin.api_key + '&all=1')
         data = json.loads(res.data)
         assert res.status_code == 200, data
-        assert data[0]['product'] == 'test_product', data
         assert data[0]['short_name'] == 'test-app1', data
 
-
     @with_context
-    def test_project_details_get_by_id_1(self):
-        """ Test get by id when result exists"""
+    def test_project_locks_get_by_id(self):
+        """ Test get locks by project id when project id exists"""
         admin = UserFactory.create(admin=True)
         project1 = self.setupProjects()
 
         # Test get by id
-        res = self.app.get('/api/projectdetails?id=' + str(project1.id) + '&api_key=' + admin.api_key + '&all=1')
+        res = self.app.get('/api/locks?id=' + str(project1.id) + '&api_key=' + admin.api_key + '&all=1')
         data = json.loads(res.data)
         assert res.status_code == 200, data
         assert len(data) == 1, data
-        assert data[0]['product'] == 'test_product', data
         assert data[0]['short_name'] == 'test-app1', data
+        assert data[0]['locks'] == [], data
 
     @with_context
-    def test_project_details_get_by_id_2(self):
-        """ Test get by id when result exists"""
-        admin = UserFactory.create(admin=True)
-        project1 = self.setupProjects()
-
-        # Test get by id
-        res = self.app.get('/api/projectdetails/' + str(project1.id) + '?api_key=' + admin.api_key)
-        data = json.loads(res.data)
-        assert res.status_code == 200, data
-        assert data['product'] == 'test_product', data
-        assert data['short_name'] == 'test-app1', data
-
-    @with_context
-    def test_project_details_get_by_product(self):
-        """ Test search by product when result exists"""
-        admin = UserFactory.create(admin=True)
-        project1 = self.setupProjects()
-
-        # Test get by product
-        res = self.app.get('/api/projectdetails?info=product::' + project1.info['product'] + '&api_key=' + admin.api_key + '&all=1')
-        data = json.loads(res.data)
-        assert res.status_code == 200, data
-        assert len(data) == 6, data
-        assert data[0]['product'] == 'test_product', data
-        assert data[1]['product'] == 'test_product', data
-
-    @with_context
-    def test_project_details_get_by_subproduct(self):
-        """ Test search by subproduct when result exists"""
-        admin = UserFactory.create(admin=True)
-        project1 = self.setupProjects()
-
-        # Test get by subproduct
-        res = self.app.get('/api/projectdetails?info=subproduct::' + project1.info['subproduct'] + '&api_key=' + admin.api_key + '&all=1')
-        data = json.loads(res.data)
-        assert res.status_code == 200, data
-        assert len(data) == 1, data
-        assert data[0]['product'] == 'test_product', data
-        assert data[0]['short_name'] == 'test-app1', data
-
-    @with_context
-    def test_project_details_no_params(self):
+    def test_project_locks_no_params(self):
         """ Test API project query when no search params"""
         admin = UserFactory.create(admin=True)
         project1 = self.setupProjects()
 
         # Test no params
-        res = self.app.get('/api/projectdetails?api_key=' + admin.api_key)
+        res = self.app.get('/api/locks?api_key=' + admin.api_key)
         data = json.loads(res.data)
         assert res.status_code == 200, data
         assert len(data) == 0, data
 
     @with_context
-    def test_project_details_value_does_not_match(self):
+    def test_project_locks_value_does_not_match(self):
         """ Test API project query when search value does not match"""
         admin = UserFactory.create(admin=True)
         project1 = self.setupProjects()
 
         # Test value DNE
-        res = self.app.get('/api/projectdetails?id=' + '9999' + '&api_key=' + admin.api_key + '&all=1')
+        res = self.app.get('/api/locks?id=' + '9999' + '&api_key=' + admin.api_key + '&all=1')
         data = json.loads(res.data)
         assert res.status_code == 200, data
         assert len(data) == 0, data
 
-
     @with_context
-    def test_project_details_param_does_not_exist(self):
-        """ Test API project query when search value does not match"""
+    def test_project_locks_param_does_not_exist(self):
+        """ Test API project query when param does not exist"""
         admin = UserFactory.create(admin=True)
         project1 = self.setupProjects()
 
         # Test bad param
-        res = self.app.get('/api/projectdetails?fakeparam=product::' + project1.info['product'] + '&api_key=' + admin.api_key + '&all=1')
+        res = self.app.get('/api/locks?fakeparam=product::' + project1.info['product'] + '&api_key=' + admin.api_key + '&all=1')
         err = json.loads(res.data)
         assert res.status_code == 415, data
         assert err['status'] == 'failed', err
@@ -212,14 +209,21 @@ class TestProjectAPI(TestAPI):
         assert err['action'] == 'GET', err
 
     @with_context
-    def test_project_details_multiple_params(self):
-        """ Test API project query when result exists"""
-        admin = UserFactory.create(admin=True)
-        project1 = self.setupProjects()
+    def test_project_locks_no_query_result(self):
+        """ Test API locks when len(query_result) == 1 and query_result[0] is None"""
+        query_result = [None]
+        try:
+            ProjectLocksAPI._create_json_response(self, query_result, None)
+        except Exception as ex:
+            assert '404' in str(ex), ex
 
-        # Test get by product and subproduct
-        res = self.app.get('/api/projectdetails?info=product::' + project1.info['product']  + '&info=subproduct::' + project1.info['subproduct'] + '&api_key=' + admin.api_key + '&all=1')
-        data = json.loads(res.data)
-        assert res.status_code == 200, data
-        assert data[0]['product'] == 'test_product', data
-        assert data[0]['subproduct'] == 'test_subproduct1', data
+    @patch('json.dumps')
+    def test_project_locks_oid_not_none(self, mock_json_dumps):
+        """ Test API locks when oid is not None"""
+        query_result = ['item1', 'item2']
+        oid = '123'
+        expected_result = 'item1'
+        mock_json_dumps.return_value = expected_result
+
+        result = ProjectLocksAPI._create_json_response(self, query_result, oid)
+        assert result == 'item1', result
