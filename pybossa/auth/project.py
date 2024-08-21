26b19170e6470d2535ffc8c05b0d4cf24baeb455
nsyed22 <noorsyed1a@gmail.com>
2024-08-21 10:27:14 -0400

    cleanup

--- pybossa/auth/project.py
+++ pybossa/auth/project.py
@@ -41,10 +41,6 @@ class ProjectAuth(object):
         return user.is_authenticated and \
             user.id in project.info.get('project_users', [])
 
-    @staticmethod
-    def users_assigned_to_project(project):
-        return len(project.info.get('project_users', [])) > 0
-
     def can(self, user, action, project=None):
         action = ''.join(['_', action])
         return getattr(self, action)(user, project)
@@ -60,7 +56,7 @@ class ProjectAuth(object):
                 return self.only_admin_or_subadminowner(user, project)
             # private gigwork
             if data_access.data_access_levels or \
-                (not data_access.data_access_levels and self.users_assigned_to_project(project)): # public gigwork and no users assigned to project
+                (not data_access.data_access_levels and len(project.get_project_users()) > 0): # public gigwork and no users assigned to project
                 return self.only_admin_or_subadminowner(user, project) or \
                     self.only_project_users(user, project)
         return user.is_authenticated
