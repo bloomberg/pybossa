70f77c38d64809c25fa06d2e422f5609fb703cf3
dchhabda <DCHHABDA@bloomberg.net>
2025-07-24 12:42:21 -0400

    cleanup, add tests

--- test/test_view/test_fileproxy.py
+++ test/test_view/test_fileproxy.py
@@ -19,6 +19,7 @@
 
 import os
 from test import with_context
+from nose.tools import assert_raises
 import json
 from test.helper import web
 from unittest.mock import patch, MagicMock
@@ -26,6 +27,7 @@ from test.factories import ProjectFactory, TaskFactory, UserFactory
 from pybossa.core import signer
 from pybossa.encryption import AESWithGCM
 from boto.exception import S3ResponseError
+from pybossa.task_creator_helper import get_path, get_secret_from_env
 
 
 class TestFileproxy(web.Helper):
@@ -270,213 +272,6 @@ class TestFileproxy(web.Helper):
 
         res = self.app.get(req_url, follow_redirects=True)
         assert res.status_code == 404, res.status_code
-
-
-# class TestHDFSproxy(web.Helper):
-
-#     app_config = {
-#         'HDFS_CONFIG': {
-#             'test': {
-#                 'url': 'https://webhdfs.ie',
-#                 'user': 'testuser',
-#                 'keytab': './oi'
-#             }
-#         },
-#         'VAULT_CONFIG': {
-#             'url': 'https://valut.com/{key_id}',
-#             'request': {
-#                 'headers': {'Authorization': 'apikey'}
-#             },
-#             'response': ['key'],
-#             'error': ['error']
-#         },
-#         'ENCRYPTION_CONFIG_PATH': ['ext_config', 'encryption']
-#     }
-
-#     @with_context
-#     def test_proxy_no_config(self):
-#         project = ProjectFactory.create()
-#         owner = project.owner
-
-#         url = '/fileproxy/hdfs/test/%s/file.pdf?api_key=%s' \
-#              % (project.id, owner.api_key)
-#         res = self.app.get(url, follow_redirects=True)
-#         assert res.status_code == 404, res.status_code
-
-#     @with_context
-#     def test_proxy_no_signature(self):
-#         project = ProjectFactory.create()
-#         owner = project.owner
-
-#         url = '/fileproxy/hdfs/test/%s/file.pdf?api_key=%s' \
-#              % (project.id, owner.api_key)
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(url, follow_redirects=True)
-#             assert res.status_code == 403, res.status_code
-
-#     @with_context
-#     def test_proxy_invalid_signature(self):
-#         """invalid signature beyond max length (128)"""
-#         import string
-#         import random
-
-#         project = ProjectFactory.create()
-#         owner = project.owner
-
-#         task_id = 2020127
-#         signature = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(129))
-#         url = '/fileproxy/hdfs/test/%s/file.pdf?api_key=%s&task-signature=%s' \
-#             % (project.id, owner.api_key, signature)
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(url, follow_redirects=True)
-#             assert res.status == '403 FORBIDDEN', res.status_code
-
-#     @with_context
-#     def test_proxy_no_task(self):
-#         project = ProjectFactory.create()
-#         owner = project.owner
-
-#         signature = signer.dumps({'task_id': 100})
-
-#         url = '/fileproxy/hdfs/test/%s/file.pdf?api_key=%s&task-signature=%s' \
-#             % (project.id, owner.api_key, signature)
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(url, follow_redirects=True)
-#             assert res.status_code == 400, res.status_code
-
-#     @with_context
-#     @patch('pybossa.view.fileproxy.HDFSKerberos.get')
-#     @patch('pybossa.view.fileproxy.requests.get')
-#     def test_proxy_owner(self, http_get, hdfs_get):
-#         res = MagicMock()
-#         res.json.return_value = {'key': 'testkey'}
-#         http_get.return_value = res
-
-#         project = ProjectFactory.create(info={
-#             'ext_config': {
-#                 'encryption': {'key_id': 123}
-#             }
-#         })
-#         url = '/fileproxy/hdfs/test/%s/file.pdf' % project.id
-#         task = TaskFactory.create(project=project, info={
-#             'url': url
-#         })
-#         owner = project.owner
-
-#         signature = signer.dumps({'task_id': task.id})
-#         req_url = '%s?api_key=%s&task-signature=%s' % (url, owner.api_key, signature)
-
-#         encryption_key = 'testkey'
-#         aes = AESWithGCM(encryption_key)
-#         hdfs_get.return_value = aes.encrypt('the content')
-
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(req_url, follow_redirects=True)
-#             assert res.status_code == 200, res.status_code
-#             assert res.data == b'the content', res.data
-
-#     @with_context
-#     @patch('pybossa.view.fileproxy.HDFSKerberos.get')
-#     @patch('pybossa.view.fileproxy.requests.get')
-#     def test_proxy_admin(self, http_get, hdfs_get):
-#         res = MagicMock()
-#         res.json.return_value = {'key': 'testkey'}
-#         http_get.return_value = res
-
-#         admin, owner = UserFactory.create_batch(2)
-#         project = ProjectFactory.create(owner=owner, info={
-#             'ext_config': {
-#                 'encryption': {'key_id': 123}
-#             }
-#         })
-#         url = '/fileproxy/hdfs/test/%s/file.pdf' % project.id
-#         task = TaskFactory.create(project=project, info={
-#             'url': url
-#         })
-
-#         signature = signer.dumps({'task_id': task.id})
-#         req_url = '%s?api_key=%s&task-signature=%s' % (url, admin.api_key, signature)
-
-#         encryption_key = 'testkey'
-#         aes = AESWithGCM(encryption_key)
-#         hdfs_get.return_value = aes.encrypt('the content')
-
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(req_url, follow_redirects=True)
-#             assert res.status_code == 200, res.status_code
-#             assert res.data == b'the content', res.data
-
-#     @with_context
-#     @patch('pybossa.view.fileproxy.HDFSKerberos.get')
-#     @patch('pybossa.view.fileproxy.requests.get')
-#     def test_proxy_key_err(self, http_get, hdfs_get):
-#         res = MagicMock()
-#         res.json.return_value = {'error': 'an error occurred'}
-#         http_get.return_value = res
-
-#         admin, owner = UserFactory.create_batch(2)
-#         project = ProjectFactory.create(owner=owner, info={
-#             'ext_config': {
-#                 'encryption': {'key_id': 123}
-#             }
-#         })
-#         url = '/fileproxy/hdfs/test/%s/file.pdf' % project.id
-#         task = TaskFactory.create(project=project, info={
-#             'url': url
-#         })
-
-#         signature = signer.dumps({'task_id': task.id})
-#         req_url = '%s?api_key=%s&task-signature=%s' % (url, admin.api_key, signature)
-
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(req_url, follow_redirects=True)
-#             assert res.status_code == 500, res.status_code
-
-#     @with_context
-#     @patch('pybossa.view.fileproxy.HDFSKerberos.get')
-#     @patch('pybossa.view.fileproxy.requests.get')
-#     def test_offset_length(self, http_get, hdfs_get):
-#         res = MagicMock()
-#         res.json.return_value = {'key': 'testkey'}
-#         http_get.return_value = res
-
-#         project = ProjectFactory.create(info={
-#             'ext_config': {
-#                 'encryption': {'key_id': 123}
-#             }
-#         })
-#         url = '/fileproxy/hdfs/test/%s/file.ndjson?offset=10&length=10' % project.id
-#         task = TaskFactory.create(project=project, info={
-#             'url': url
-#         })
-#         owner = project.owner
-
-#         signature = signer.dumps({'task_id': task.id})
-#         req_url = '%s&api_key=%s&task-signature=%s' % (url, owner.api_key, signature)
-
-#         encryption_key = 'testkey'
-#         aes = AESWithGCM(encryption_key)
-#         hdfs_get.return_value = aes.encrypt('the content')
-
-#         with patch.dict(self.flask_app.config, self.app_config):
-#             res = self.app.get(req_url, follow_redirects=True)
-#             assert res.status_code == 200, res.status_code
-#             assert res.data == b'the content', res.data
-
-
-# def test_is_file_url():
-#     file_url = '/a/b'
-#     qps = {'offset': ['1'], 'length': ['2']}
-#     is_valid_url = is_valid_hdfs_url(file_url, qps)
-#     assert not is_valid_url(1)
-#     assert not is_valid_url('/a/c')
-#     assert not is_valid_url('/a/b?offset=1')
-#     assert not is_valid_url('/a/b?offset=1&offset=2')
-#     assert not is_valid_url('/a/b?length=2')
-#     assert not is_valid_url('/a/b?offset=1&length=3')
-#     assert not is_valid_url('/a/b?offset=1&length=3&task-signature=asdfasdfad')
-#     assert is_valid_url('/a/b?offset=1&length=2')
-#     assert is_valid_url('/a/b?offset=1&length=2&task-signature=asdfasdfas')
 
 
 class TestEncryptedPayload(web.Helper):
@@ -787,3 +582,71 @@ class TestEncryptedPayload(web.Helper):
             resp = self.app.get(url, follow_redirects=True)
             assert resp.status == '500 INTERNAL SERVER ERROR', resp.status_code
             assert 'SECRET_CONFIG_ENV' not in self.flask_app.config
+
+    def test_empty_path_returns_dict(self):
+        d = {'a': 1}
+        assert get_path(d, []) ==  d
+
+    def test_single_level_path(self):
+        d = {'a': 1, 'b': 2}
+        assert get_path(d, ['a']) == 1
+        assert get_path(d, ['b']) == 2
+
+    def test_multi_level_path(self):
+        d = {'a': {'b': {'c': 42}}}
+        assert get_path(d, ['a', 'b', 'c']) == 42
+
+    def test_path_not_found_raises(self):
+        d = {'a': {'b': 2}}
+        with assert_raises(KeyError):
+            get_path(d, ['a', 'c'])
+
+    def test_non_dict_in_path_raises(self):
+        d = {'a': 1}
+        with assert_raises(TypeError):
+            get_path(d, ['a', 'b'])
+
+    @with_context
+    def test_get_secret_from_env_success(self):
+        app_config = {
+            'SECRET_CONFIG_ENV': {'secret_id_prefix': 'key_id'}
+        }
+        with patch.dict(self.flask_app.config, app_config):
+            project_encryption = {'key_id': '123'}
+            env_key = 'key_id_123'
+            os.environ[env_key] = 'supersecret'
+            assert get_secret_from_env(project_encryption) == 'supersecret'
+            del os.environ[env_key]
+
+    @with_context
+    def test_get_secret_from_env_invalid_config_type(self):
+        app_config = {
+            'SECRET_CONFIG_ENV': 'not_a_dict'
+        }
+        with patch.dict(self.flask_app.config, app_config):
+            project_encryption = {'key_id': '123'}
+            with assert_raises(RuntimeError):
+                get_secret_from_env(project_encryption)
+
+    @with_context
+    def test_get_secret_from_env_missing_secret_id_prefix(self):
+        app_config = {
+            'SECRET_CONFIG_ENV': {}
+        }
+        with patch.dict(self.flask_app.config, app_config):
+            project_encryption = {'key_id': '123'}
+            with assert_raises(RuntimeError):
+                get_secret_from_env(project_encryption)
+
+    @with_context
+    def test_get_secret_from_env_env_key_missing(self):
+        app_config = {
+            'SECRET_CONFIG_ENV': {'secret_id_prefix': 'key_id'}
+        }
+        with patch.dict(self.flask_app.config, app_config):
+            project_encryption = {'key_id': 'not_in_env'}
+            env_key = 'key_id_not_in_env'
+            if env_key in os.environ:
+                del os.environ[env_key]
+            with assert_raises(RuntimeError):
+                get_secret_from_env(project_encryption)
