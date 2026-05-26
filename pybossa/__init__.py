4787fc4f24134216a3e84d2e4f8195a038e9df4b
dchhabda <DCHHABDA@bloomberg.net>
2026-05-26 09:24:26 -0400

    fix boto2 six module broken in py3.12

--- pybossa/__init__.py
+++ pybossa/__init__.py
@@ -26,4 +26,37 @@ This exports:
 """
 
 
+# Patch boto v2 for Python 3.12+ compatibility.
+# boto v2 uses 'imp' (removed in 3.12) and a broken vendored 'six'.
+# This must run before any boto import.
+import sys
+import importlib
+import importlib.util
+import types
+
+# Provide a shim for the removed 'imp' module that boto.plugin uses.
+if 'imp' not in sys.modules:
+    imp_shim = types.ModuleType('imp')
+    imp_shim.find_module = lambda name, path=None: (None, None, None)
+    imp_shim.load_module = lambda *args, **kwargs: None
+    sys.modules['imp'] = imp_shim
+
+# Patch boto's broken vendored six.
+import six
+import six.moves
+import six.moves.urllib
+import six.moves.urllib.parse
+import six.moves.urllib.error
+import six.moves.urllib.request
+import queue
+import http.client
+sys.modules['boto.vendored.six'] = six
+sys.modules['boto.vendored.six.moves'] = six.moves
+sys.modules['boto.vendored.six.moves.urllib'] = six.moves.urllib
+sys.modules['boto.vendored.six.moves.urllib.parse'] = six.moves.urllib.parse
+sys.modules['boto.vendored.six.moves.urllib.error'] = six.moves.urllib.error
+sys.modules['boto.vendored.six.moves.urllib.request'] = six.moves.urllib.request
+sys.modules['boto.vendored.six.moves.queue'] = queue
+sys.modules['boto.vendored.six.moves.http_client'] = http.client
+
 __version__ = "2.9.2"  # pragma: no cover
